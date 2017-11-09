b1fc08012e77638e3f324cf5368144282715591f
hariharnath <hariharnath919@gmail.com>
2017-11-09 13:09:50 +0530

    Modified code to loop the Temp and Humidity

--- testDHT11.py
+++ testDHT11.py
@@ -8,9 +8,14 @@ sensor_pin = 4
 
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-else:
-    print('Failed to get reading. Try checking the pin numbering and connections!')
+while True:
+	# Try to grab a sensor reading.  Use the read_retry method which will retry up
+	# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+	if humidity is not None and temperature is not None:
+		print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+	else:
+		print('Failed to get reading. Try checking the pin numbering and connections!')
+			

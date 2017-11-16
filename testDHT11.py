4b107233a07857dd4c6665ccd51ff1778e23759f
hariharnath <hariharnath919@gmail.com>
2017-11-16 17:38:29 +0530

    Update testDHT11.py

--- testDHT11.py
+++ testDHT11.py
@@ -1,5 +1,5 @@
 import Adafruit_DHT
-
+import time
 # Sensor should be set to Adafruit_DHT.DHT11,
 sensor = Adafruit_DHT.DHT11
 
@@ -12,10 +12,12 @@ sensor_pin = 4
 while True:
 	# Try to grab a sensor reading.  Use the read_retry method which will retry up
 	# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+	humidity, temperature = Adafruit_DHT.read_retry(sensor, sensor_pin)
 
 	if humidity is not None and temperature is not None:
 		print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+		time.sleep(1)
 	else:
 		print('Failed to get reading. Try checking the pin numbering and connections!')
 			
+		
